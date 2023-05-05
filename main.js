005a05aff481d6798c9d4e38246ab9af432e3942
thaty cacezes <thaty_cacezes@hotmail.com>
2023-05-05 09:04:28 +0200

    base de donees

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function openPopup() {
-  var popup = window.open("create_account.php", "popup", "width=400, height=400");
+    // Affiche la fenêtre modale
+    // ...
+}
+
+// Récupère la fenêtre modale
+var modal = document.getElementById("myModal");
+
+// Récupère le bouton de fermeture de la fenêtre modale
+var span = document.getElementsByClassName("close")[0];
+
+// Ferme la fenêtre modale lorsqu'on clique sur le bouton de fermeture
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Ferme la fenêtre modale lorsqu'on clique en dehors de la fenêtre
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
--- register.php
+++ register.php
@@ -1,35 +1,68 @@
-<?php
-// Informations de connexion à la base de données MySQL
+<?
+// Connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "Quai_Antique";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Connexion à la base de données
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-	die("Connexion échouée : " . mysqli_connect_error());
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Récupération des données du formulaire
-$username = mysqli_real_escape_string($conn, $_POST['username']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
+// Traitement du formulaire de connexion
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-// Insertion des données dans la table 'users'
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
-if (mysqli_query($conn, $sql)) {
-	echo "Compte créé avec succès !";
-} else {
-	echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+    // Vérification des identifiants de connexion dans la base de données
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        // Connexion réussie
+        // Rediriger vers la page d'accueil
+        header("Location: index.html");
+    } else {
+        // Identifiants de connexion invalides
+        // Afficher un message d'erreur
+        echo "Nom d'utilisateur ou mot de passe incorrect.";
+    }
 }
 
-// Fermeture de la connexion à la base de données
-mysqli_close($conn);
+// Traitement du formulaire de création de compte
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["createAccount"])) {
+    $newUsername = $_POST["newUsername"];
+    $newEmail = $_POST["newEmail"];
+    $newPassword = $_POST["newPassword"];
+
+    // Vérification si le nom d'utilisateur ou l'adresse email existe déjà dans la base de données
+    $sql = "SELECT * FROM users WHERE username='$newUsername' OR email='$newEmail'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Nom d'utilisateur ou adresse email déjà utilisé
+        // Afficher un message d'erreur
+        echo "Le nom d'utilisateur ou l'adresse email existe déjà.";
+    } else {
+        // Ajouter le nouvel utilisateur à la base de données
+        $sql = "INSERT INTO users (username, email, password) VALUES ('$newUsername', '$newEmail', '$newPassword')";
+        if ($conn->query($sql) === TRUE) {
+            // Rediriger vers la page de connexion
+            header("Location: login.php");
+        } else {
+            // Erreur lors de l'ajout de l'utilisateur
+            // Afficher un message d'erreur
+            echo "Erreur : " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+// Fermer la connexion à la base de données
+$conn->close();
+
 ?>
-
-
-
 
 
 
