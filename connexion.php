b7f6195ae7917ec38ac8811a389880f55842a10e
thaty cacezes <thaty_cacezes@hotmail.com>
2023-05-05 11:01:37 +0200

    connexion bd

---
+++ connexion.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+  header("Location: index.html"); // Rediriger l'utilisateur s'il est déjà connecté
+  exit();
+}
+
+// Vérifier si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  // Connexion à la base de données avec PDO
+  $servername = "localhost";
+  $dbname = "Quai_Antique";
+  $username_db = "root";
+  $password_db = "root";
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username_db, $password_db);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Vérifier si les informations d'identification sont correctes
+    $stmt = $conn->prepare("SELECT * FROM utilisateurs WHERE username = :username AND password = :password");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result !== false) { // Si les informations d'identification sont correctes
+      $_SESSION['username'] = $username;
+      header("Location: index.html");
+      exit();
+    } else { // Si les informations d'identification sont incorrectes
+      $error_message = "Nom d'utilisateur ou mot de passe incorrect";
+    }
+  } catch(PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+  }
+  $conn = null;
+}
+?>
+
--- register.php
+++ register.php
@@ -1,69 +1,36 @@
-<?
+<?php
 // Connexion à la base de données
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "Quai_Antique";
+$username = "nom_utilisateur";
+$password = "mot_de_passe";
+$dbname = "nom_de_la_base_de_données";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérification de la connexion
+// Vérifier si la connexion a réussi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Traitement du formulaire de connexion
+// Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+  // Récupérer les données du formulaire
+  $email = $_POST["email"];
+  $id = $_POST["id"];
+  $password = $_POST["password"];
 
-    // Vérification des identifiants de connexion dans la base de données
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = $conn->query($sql);
+  // Préparer et exécuter la requête SQL d'insertion
+  $sql = "INSERT INTO utilisateurs (email, id, password) VALUES ('$email', '$id', '$password')";
 
-    if ($result->num_rows == 1) {
-        // Connexion réussie
-        // Rediriger vers la page d'accueil
-        header("Location: index.html");
-    } else {
-        // Identifiants de connexion invalides
-        // Afficher un message d'erreur
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
-    }
-}
-
-// Traitement du formulaire de création de compte
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["createAccount"])) {
-    $newUsername = $_POST["newUsername"];
-    $newEmail = $_POST["newEmail"];
-    $newPassword = $_POST["newPassword"];
-
-    // Vérification si le nom d'utilisateur ou l'adresse email existe déjà dans la base de données
-    $sql = "SELECT * FROM users WHERE username='$newUsername' OR email='$newEmail'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Nom d'utilisateur ou adresse email déjà utilisé
-        // Afficher un message d'erreur
-        echo "Le nom d'utilisateur ou l'adresse email existe déjà.";
-    } else {
-        // Ajouter le nouvel utilisateur à la base de données
-        $sql = "INSERT INTO users (username, email, password) VALUES ('$newUsername', '$newEmail', '$newPassword')";
-        if ($conn->query($sql) === TRUE) {
-            // Rediriger vers la page de connexion
-            header("Location: login.php");
-        } else {
-            // Erreur lors de l'ajout de l'utilisateur
-            // Afficher un message d'erreur
-            echo "Erreur : " . $sql . "<br>" . $conn->error;
-        }
-    }
+  if ($conn->query($sql) === TRUE) {
+    echo "Nouvel utilisateur créé avec succès";
+  } else {
+    echo "Erreur: " . $sql . "<br>" . $conn->error;
+  }
 }
 
 // Fermer la connexion à la base de données
 $conn->close();
-
 ?>
-
-
 
 
